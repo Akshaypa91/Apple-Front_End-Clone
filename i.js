c60a7c702ccaa2e44f05a9140365cf2f5ba2f2f5
Akshaypa91 <pagareakshay1294@gmail.com>
2025-04-29 09:27:24 +0530

    fix js

--- i.js
+++
@@ -1,3 +0,0 @@
-const container = document.getElementById('productsContainer');
-document.getElementById('scrollLeft').onclick = () => container.scrollBy({ left: -300, behavior: 'smooth' });
-document.getElementById('scrollRight').onclick = () => container.scrollBy({ left: 300, behavior: 'smooth' });``
